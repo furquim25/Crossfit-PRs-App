34ad4d8ff4b10fe86cfa802e42fbe14833e5bfa5
Rafael Furquim <rafa_furquim@nidf.ufrj.br>
2023-02-21 21:13:40 -0300

    added model for items

--- crossfitapp/core/models.py
+++ crossfitapp/core/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    observation = models.TextField(blank=True, null=True)
+    personal_record = models.FloatField()
+    pr_date = models.DateField()
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name + f" PR {self.personal_record} ({self.created_by})"
--- crossfitapp/core/views.py
+++ crossfitapp/core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from core.models import Item
+
 # Create your views here.
 def index(request):
-     return render(request, 'core/index.html')
+     items = Item.objects.all()
+     return render(request, 'core/index.html', {
+          'items': items
+     })
  
 def info(request):
     return render(request, 'core/info.html')
